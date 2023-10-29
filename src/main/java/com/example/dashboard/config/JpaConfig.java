3605d560049b4323c01f21377a70492fe144a801
sayya <djdj9876@naver.com>
2023-10-30 02:43:18 +0900

    feat: JpaConfig 파일 구현(Auditing 기능)

--- src/main/java/com/example/dashboard/config/JpaConfig.java
+++ src/main/java/com/example/dashboard/config/JpaConfig.java
@@ -1,2 +1,20 @@
-package com.example.dashboard.config;public class JpaConfig {
+package com.example.dashboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("sayya");
+        // TODO: Spring Security 설정 이후 리팩토링 필요
+    }
+
 }
